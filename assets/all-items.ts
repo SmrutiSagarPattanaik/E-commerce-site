40d79b8447d679353594aa00016e3bbb777846ed
SmrutiSagarPattanaik <mr.pattanaik.1997@gmail.com>
2021-06-21 09:21:28 +0530

    Auto-generated commit [ci skip]

---
+++ assets/all-items.ts
@@ -0,0 +1,35 @@
+export let itemDetailsList= [
+    {
+      imageUrl:
+        'https://previews.123rf.com/images/kantver/kantver1601/kantver160100176/50793311-smartphone-holding-in-female-hand-on-the-blured-background.jpg',
+      id: 'E1',
+      name: 'Mobile',
+      description: 'This is a mobile device',
+      price: 37000,
+    },
+    {
+      imageUrl:
+        'https://previews.123rf.com/images/kalinavova/kalinavova1601/kalinavova160100026/50626443-modern-laptop-isolated-on-white.jpg',
+      id: 'E2',
+      name: 'Laptop',
+      description: 'This is a Laptop',
+      price: 80000,
+    },
+    {
+      imageUrl:
+        'https://previews.123rf.com/images/utima/utima1512/utima151200002/50416913-three-fresh-red-apples-with-drops-isolated-on-white.jpg',
+      id: 'V1',
+      name: 'Apple',
+      description: 'This is apple',
+      price: 250,
+    },
+    {
+      imageUrl:
+        'https://previews.123rf.com/images/olegdudko/olegdudko1511/olegdudko151105767/47575036-milk-.jpg',
+      id: 'D1',
+      name: 'Milk',
+      description: 'This is Milk',
+      price: 25,
+    },
+  ];
+  
